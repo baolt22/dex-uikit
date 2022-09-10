ed70e0f8bcdd09b533122d8d77500eea51a0d6a1
luthebao <luthebao97@gmail.com>
2022-09-10 17:52:04 +0700

    1.1.2

--- src/widgets/Navbar/index.tsx
+++ src/widgets/Navbar/index.tsx
@@ -1,3 +1,5 @@
 export { default as Navbar } from "./Navbar";
 export { links as menuConfig } from "./config";
+export { default as NetworkButton } from "./NetworkButton"
+export { default as LangSelector } from "../../components/LangSelector/LangSelector";
 export type { NavProps, LangType, MenuEntry, MenuSubEntry } from "./types";
